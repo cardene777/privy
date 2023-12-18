92136453b29f269be8f2839f0a5b0ffe13b046cf
cardene <chotips08@gmail.com>
2023-12-19 01:48:57 +0900

    🚀 connect multiple wallets

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { usePrivy } from "@privy-io/react-auth";
+import { usePrivy, useWallets } from "@privy-io/react-auth";
 
 export default function Profile() {
   const {
@@ -22,70 +22,72 @@ export default function Profile() {
     unlinkDiscord,
     unlinkApple,
     unlinkPhone,
+    connectWallet,
   } = usePrivy();
-    const router = useRouter();
+  const { wallets } = useWallets();
+  const router = useRouter();
 
-    const btnObject = [
-        {
-            name: 'wallet',
-            func: unlinkWallet,
-        },
-        {
-            name: 'email',
-            func: unlinkEmail,
-        },
-        {
-            name: 'github',
-            func: unlinkGithub,
-        },
-        {
-            name: 'google',
-            func: unlinkGoogle,
-        },
-        {
-            name: 'discord',
-            func: unlinkDiscord,
-        },
-        {
-            name: 'apple',
-            func: unlinkApple,
-        },
-        {
-            name: 'phone',
-            func: unlinkPhone,
-        },
-    ]
+  const btnObject = [
+    {
+      name: "wallet",
+      func: unlinkWallet,
+    },
+    {
+      name: "email",
+      func: unlinkEmail,
+    },
+    {
+      name: "github",
+      func: unlinkGithub,
+    },
+    {
+      name: "google",
+      func: unlinkGoogle,
+    },
+    {
+      name: "discord",
+      func: unlinkDiscord,
+    },
+    {
+      name: "apple",
+      func: unlinkApple,
+    },
+    {
+      name: "phone",
+      func: unlinkPhone,
+    },
+  ];
 
-    const unlinkBtnObject = [
-      {
-        name: "unlinkWallet",
-        func: linkWallet,
-      },
-      {
-        name: "unlinkEmail",
-        func: linkEmail,
-      },
-      {
-        name: "unlinkGithub",
-        func: linkGithub,
-      },
-      {
-        name: "unlinkGoogle",
-        func: linkGoogle,
-      },
-      {
-        name: "unlinkDiscord",
-        func: linkDiscord,
-      },
-      {
-        name: "unlinkApple",
-        func: linkApple,
-      },
-      {
-        name: "unlinkPhone",
-        func: linkPhone,
-      },
-    ];
+  const unlinkBtnObject = [
+    {
+      name: "unlinkWallet",
+      func: linkWallet,
+    },
+    {
+      name: "unlinkEmail",
+      func: linkEmail,
+    },
+    {
+      name: "unlinkGithub",
+      func: linkGithub,
+    },
+    {
+      name: "unlinkGoogle",
+      func: linkGoogle,
+    },
+    {
+      name: "unlinkDiscord",
+      func: linkDiscord,
+    },
+    {
+      name: "unlinkApple",
+      func: linkApple,
+    },
+    {
+      name: "unlinkPhone",
+      func: linkPhone,
+    },
+  ];
 
   if (!(ready && authenticated) || !user) {
     return null;
@@ -132,18 +134,27 @@ export default function Profile() {
             ))}
           </div>
         </div>
-        <div>
+        <div className="mt-12">
           <div className="flex justify-center text-center space-x-3">
             {unlinkBtnObject.map((item, index) => (
               <button
                 key={index}
-                className="text-center font-semibold text-lg mt-12 border-2 rounded-lg py-1 px-3"
+                className="text-center font-semibold text-lg border-2 rounded-lg py-1 px-3"
                 onClick={item.func}
               >
                 {item.name}
               </button>
             ))}
           </div>
+        </div>
+        <div className="mt-12 flex-col justify-center text-center space-x-3">
+          <button
+            onClick={connectWallet}
+            className="text-center font-semibold text-lg border-2 rounded-lg py-1 px-3"
+          >
+            Connect Wallet
+          </button>
+          <p className="mt-2">Total connected wallets: {wallets.length}</p>
         </div>
       </div>
     </div>
