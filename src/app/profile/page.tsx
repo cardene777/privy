157b5104d2cebb40095bfc69b39fd6c778352066
cardene <chotips08@gmail.com>
2023-12-19 00:05:41 +0900

    🚀 authentication logout

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,28 @@
 "use client";
+import { useRouter } from "next/navigation";
+import { usePrivy } from "@privy-io/react-auth";
+
 
 export default function Profile() {
+    const { logout } = usePrivy();
+    const router = useRouter();
+
+    const logoutHandler = () => {
+        logout();
+        router.push("/");
+    }
+
   return (
-      <>
-        <h1>Profile</h1>
-      </>
+    <div className="container mx-auto w-full h-full mt-20">
+      <p className="text-center font-bold text-5xl">Profile</p>
+      <div className="flex justify-center">
+        <button
+          className="text-center font-semibold text-lg mt-12 border-2 rounded-lg py-1 px-3"
+          onClick={logoutHandler}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
   );
 }
